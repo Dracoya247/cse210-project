0f1251b14d88a2d22309fd09f5feee954fcde186
Aria Menke <aria_menke@byui.edu>
2021-03-30 13:59:18 -0700

    Letters and Score update.

--- project_template/Milkboy/milkboy_beta.py
+++ project_template/Milkboy/milkboy_beta.py
@@ -346,8 +346,8 @@ class MyGame(arcade.Window):
 
         # Draw our score on the screen, scrolling it with the viewport
         score_text = f"Score: {self.score}"
-        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
-                         arcade.csscolor.WHITE, 18)
+        arcade.draw_text(score_text, 10 + self.view_left, 600 + self.view_bottom,
+                         arcade.csscolor.BLACK, 18)
 
         # Draw hit boxes.
         # for wall in self.wall_list:
